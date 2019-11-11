588fceb7eefa2f0bc3925f5a138d522bd47f5f71
Paul McManus <paulmcmanus2021@hotmail.com>
2019-11-11 19:56:04 +0000

    Pre attr_accessor

--- team/specs/team_specs.rb
+++ team/specs/team_specs.rb
@@ -3,6 +3,35 @@ require_relative ('../team.rb')
 
 class TestTeam < MiniTest::Test
   def test_create_team
-    team = Team.new("Green Bay Packers",["Aaron Rodgers","Aaron Jones", "Davante Adams"],"Matt LeFleur")
+    team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
   end
+
+
+#getters
+
+  def test_team_name
+    my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
+    assert_equal("TeamName", my_team.team_name)
+  end
+
+  def test_players
+    my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
+    assert_equal(["Player1","Player2","Player3"], my_team.players)
+  end
+
+  def test_coach
+    my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
+    assert_equal("Coach1", my_team.coach)
+  end
+#setter
+def test_set_coach_name
+ my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
+ my_team.set_coach_name("Coach2")
+ assert_equal("Coach2", my_team.coach)
+end
+
+
+
+
+#THIS IS THE FINAL END
 end
--- team/team.rb
+++ team/team.rb
@@ -5,5 +5,24 @@ class Team
     @players = input_players
     @coach = input_coach
   end
+#getters
+  def team_name
+    return @team_name
+  end
+
+  def players
+    return @players
+  end
+
+  def coach
+    return @coach
+  end
+#setters
+  def set_coach_name(name)
+    @coach = name
+  end
+
+
+
 
 end
