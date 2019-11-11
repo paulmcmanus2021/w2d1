6056a9b4eecceb0fa56fefd506caa3009e021539
Paul McManus <paulmcmanus2021@hotmail.com>
2019-11-11 22:20:41 +0000

    After Check Players

--- team/specs/team_specs.rb
+++ team/specs/team_specs.rb
@@ -6,32 +6,45 @@ class TestTeam < MiniTest::Test
     team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
   end
 
+  #getters
+    def test_team_name
+      my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
+      assert_equal("TeamName", my_team.team_name)
+    end
 
-#getters
+    def test_players
+      my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
+      assert_equal(["Player1","Player2","Player3"], my_team.players)
+    end
 
-  def test_team_name
-    my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
-    assert_equal("TeamName", my_team.team_name)
+    def test_coach
+      my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
+      assert_equal("Coach1", my_team.coach)
+    end
+  #setter
+  def test_set_coach_name
+   my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
+   my_team.set_coach_name("Coach2")
+   assert_equal("Coach2", my_team.coach)
   end
 
-  def test_players
+  def test_add_player
     my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
-    assert_equal(["Player1","Player2","Player3"], my_team.players)
+    my_team.add_player("Player4")
+    assert_equal(["Player1","Player2","Player3","Player4"],my_team.players)
   end
 
-  def test_coach
-    my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
-    assert_equal("Coach1", my_team.coach)
+  def test_player_present__true
+    my_team = Team.new("TeamName", ["Player1", "Player2", "Player3"], "Coach1")
+    result = my_team.player_present("Player2")
+    assert_equal(true, result)
   end
-#setter
-def test_set_coach_name
- my_team = Team.new("TeamName",["Player1","Player2","Player3"],"Coach1")
- my_team.set_coach_name("Coach2")
- assert_equal("Coach2", my_team.coach)
-end
 
-
-
+  def test_player_present__false
+    my_team = Team.new("TeamName", ["Player1", "Player2", "Player3"], "Coach1")
+    result = my_team.player_present("Player6")
+    assert_equal(false, result)
+  end
 
 #THIS IS THE FINAL END
 end
--- team/team.rb
+++ team/team.rb
@@ -1,28 +1,35 @@
 class Team
 
+  attr_reader :coach, :players, :team_name
+  attr_writer :coach, :players, :team_name
   def initialize(input_team_name,input_players,input_coach)
     @team_name = input_team_name
     @players = input_players
     @coach = input_coach
   end
 #getters
-  def team_name
-    return @team_name
-  end
-
-  def players
-    return @players
-  end
-
-  def coach
-    return @coach
-  end
-#setters
+  # def team_name
+  #   return @team_name
+  # end
+  #
+  # def players
+  #   return @players
+  # end
+  #
+  # def coach
+  #   return @coach
+  # end
+ #setters
   def set_coach_name(name)
     @coach = name
   end
 
-
-
+  def add_player(name)
+    @players.push(name)
+  end
+  
+  def player_present(name)
+    @players.include?(name)
+  end
 
 end
